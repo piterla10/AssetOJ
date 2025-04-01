ddf63dfbcd690473c9e8498f5f0c647d93548e7f
ojhb1 <ojhb1@gcloud.ua.es>
2025-04-01 19:45:16 +0200

    Cambios

--- backend/router/assets.js
+++ backend/router/assets.js
@@ -5,9 +5,9 @@ const {validarJWT} = require('../middlewares/validar-jwt');
 const {obtenerAssetsPorTipo,crearAsset} = require('../controllers/assets');
 const router = Router();
 
-router.get('/', [
-    check('x-token', 'El argumento x-token es obligatorio').not().isEmpty(),
-    validarJWT,
+router.get('/:tipo', [
+    //check('x-token', 'El argumento x-token es obligatorio').not().isEmpty(),
+    //validarJWT,
     validarCampos,
 ], obtenerAssetsPorTipo);
 
