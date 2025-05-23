4dd075088b19f87dc451beaae2b1df4f33499d37
ojhb1 <ojhb1@gcloud.ua.es>
2025-05-23 22:36:40 +0200

    fd

--- frontend/src/features/comentarios/comentariosService.js
+++ frontend/src/features/comentarios/comentariosService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+const apiUrl = process.env.REACT_APP_API_URL;
 const token = localStorage.getItem('token'); // Recuperar el token almacenado
 
 const config = {
